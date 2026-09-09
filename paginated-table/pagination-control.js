555ff1efe4f9c83b6fd1256778629373b8565624
Tubes <jasonnmorrow@gmail.com>
2026-09-09 17:06:45 -0400

    showcase PageControllerV2

--- paginated-table/pagination-control.js
+++ paginated-table/pagination-control.js
@@ -1,10 +1,19 @@
 'use strict';
-import {PageController} from '@bundle-of-tubes/stateful-components';
+import {PageController, PageControllerV2} from '@bundle-of-tubes/stateful-components';
 import paginationStyle from './paginationStyle.css' with { type: "css" };
 
 customElements.define("my-pagination-control", class extends PageController {
   DEFAULT_PAGE_SIZE = 27;
   PAGE_SIZE_OPTIONS = [8, 27, 64, 125, 216];
+  connectedCallback() {
+    super.connectedCallback();
+    this.shadowRoot.adoptedStyleSheets = [paginationStyle];
+  }
+});
+
+customElements.define("my-pagination-control-v2", class extends PageControllerV2 {
+  DEFAULT_PAGE_SIZE = 8;
+  PAGE_SIZE_OPTIONS = [8, 16, 32, 64, 128, 256];
   connectedCallback() {
     super.connectedCallback();
     this.shadowRoot.adoptedStyleSheets = [paginationStyle];
--- paginated-table/script.js
+++ paginated-table/script.js
@@ -25,3 +25,28 @@ const paginator = document.getElementById('paginator');
 paginator.setAttribute('cardinality', cardinality);
 paginator.addEventListener('paginate', e=>showPage(e.skip, e.take));
 showPage(0, Math.min(cardinality, paginator.DEFAULT_PAGE_SIZE));
+
+function showPage2(skip, take) {
+  console.log(`Page on v2 table requested: skip=${skip}, take=${take}`);
+  const data2 = new Array(take);
+  for (let index=0; index < take; ++index) {
+    data2[index] = skip+index+1;
+  }
+  document.getElementById("rows2").replaceChildren(...data2.map((rowNum)=>{
+    console.log('showing rowNum', rowNum); //TODO: deleteme
+    const row = document.createElement('tr');
+    const firstCell = document.createElement('td');
+    firstCell.textContent = rowNum;
+    const secondCell = document.createElement('td');
+    secondCell.textContent = Math.pow(rowNum, 2);
+    const thirdCell = document.createElement('td');
+    thirdCell.textContent = Math.pow(rowNum, 3);
+    row.replaceChildren(firstCell, secondCell, thirdCell);
+    return row;
+  }));
+}
+
+const pageinator2 = document.getElementById('paginator2');
+paginator2.setCardinality(100);
+paginator2.addEventListener('paginate', e=>showPage2(e.skip,e.take));
+showPage2(0,paginator2.DEFAULT_PAGE_SIZE);
